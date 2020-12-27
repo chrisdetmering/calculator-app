020fa76ccaaf84c5779c3b44220740a33a92e60c
unknown <Bryan@SJ-BHERRERA-XPS.netgear.com>
2020-12-27 15:09:15 -0800

    refactor script

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-//Need to add comma seporators
-// need to limit number of displayed digits
-//make sure eveyrthing resets on clear button
-
-let runningTotal = false;
-
-const display = document.querySelector('div .output');
-const numpad = document.getElementById('numpad');
-numpad.addEventListener('click', handleInput);
-const decimal = document.getElementById('decimal');
-let isDecimalActive = true;
-let isNewOperation = false;
-
-let index = 1;
 let operation = {
 	num1: '0',
 	operator: '',
@@ -20,68 +6,55 @@ let operation = {
 	highOperator: ''
 };
 
-pushInputToDisplay('0');
+let inputIndex = 1;
+let isDecimalActive = true;
+
+const display = document.querySelector('div .output');
+display.textContent = operation.num1;
+const numpad = document.getElementById('numpad');
+numpad.addEventListener('click', handleInput);
 
 function handleInput(e) {
-	if (e.target.classList.contains('number')) {
-		let input = e.target.textContent;
-		pushInputToDisplay(input);
-	}
-	if (e.target.classList.contains('decimal') && isDecimalActive) {
-		let input = e.target.textContent;
-		pushInputToDisplay(input);
-		isDecimalActive = !isDecimalActive;
-	}
+	let inputType = e.target.classList[0];
+	let input = e.target.textContent;
+	// console.log('Type: ' + inputType);
+	// console.log('Input: ' + input);
 
-	if (e.target.classList.contains('operator')) {
-		let operator = e.target.dataset.operator;
-		if (index === 3) {
-			higherOperation(operation.num2, operation.num3);
-			operation.num3 = '0';
-			index = 2;
-		}
-		index === 1 ? (index += 1) : index;
+	if (inputType === 'number') {
+		displayInput(input);
+	}
+	if (inputType === 'decimal' && isDecimalActive) {
+		displayInput(input);
+		isDecimalActive = false;
+	}
+	if (inputType === 'operator') {
+		orderOperators(input);
 		isDecimalActive = true;
-		isNewOperation = true;
-		storeOperation(operator);
 	}
-	if (e.target.classList.contains('equals')) {
-		if (index === 3) {
-			higherOperation(operation.num2, operation.num3);
+	if (inputType === 'equals') {
+		if (operation.highOperator != '') {
+			operation.num2 = performOperation[operation.highOperator](operation.num2, operation.num3).toString();
+			display.textContent = performOperation[operation.operator](operation.num1, operation.num2).toString();
 		}
-		performOperation(operation);
-		operation.num1 = display.textContent;
-		operation.operator = '';
-		operation.num2 = '0';
-		operation.num3 = '0';
-		index = 1;
-		isNewOperation = !isNewOperation;
-		operation.highOperator = '';
+		display.textContent = performOperation[operation.operator](operation.num1, operation.num2);
+		resetCalculator(inputType);
 	}
-	if (e.target.classList.contains('clear-btn')) {
-		clearCalculator();
+	if (inputType === 'clear-btn') {
+		resetCalculator(inputType);
 	}
 }
 
-function pushInputToDisplay(input) {
-	///NEED TO MAKE INDEX 2 LIKE INDEX 2
-	if (index === 1) {
-		if (isNewOperation) {
-			console.log('you need to reset to zero first');
-			display.textContent = '0';
-			operation.num1 = '0';
-			isNewOperation = !isNewOperation;
-		}
-		if (input === '.' || operation.num1.toString().includes('.')) {
+function displayInput(input) {
+	if (inputIndex === 1) {
+		if (input === '.' || operation.num1.includes('.')) {
 			operation.num1 += input;
 		} else {
 			operation.num1 += input;
 			operation.num1 = parseInt(operation.num1, 10);
 			operation.num1 = operation.num1.toString();
 		}
-		console.log(operation.num1);
 	}
-	if (index === 2) {
+	if (inputIndex === 2) {
 		if (input === '.' || operation.num2.toString().includes('.')) {
 			operation.num2 += input;
 		} else {
@@ -89,11 +62,8 @@ function pushInputToDisplay(input) {
 			operation.num2 = parseInt(operation.num2, 10);
 			operation.num2 = operation.num2.toString();
 		}
-		console.log(operation.num2);
 	}
-	if (index === 3) {
-		// operation.num3 += input;
-		// operation.num3 = operation.num3.toString();
+	if (inputIndex === 3) {
 		if (input === '.' || operation.num3.toString().includes('.')) {
 			operation.num3 += input;
 		} else {
@@ -101,97 +71,73 @@ function pushInputToDisplay(input) {
 			operation.num3 = parseInt(operation.num3, 10);
 			operation.num3 = operation.num3.toString();
 		}
-		//higherOperation(operation.num2, operation.num3);
 	}
-	// display.textContent = operation['num' + index];
-	formatDisplayNumber(operation['num' + index]);
-	//index === 3 ? (index -= 1) : index;
-	//operation.num3 = '';
-	//operation.highOperator = '';
+
+	//Display Input
+	display.textContent = operation['num' + inputIndex];
+	console.log('Current Input: ' + operation['num' + inputIndex]);
+}
+
+function orderOperators(input) {
+	inputIndex += 1;
+	if (inputIndex === 3 && input != 'times' && (inputIndex === 3 && input != 'divide')) {
+		operation.num1 = performOperation[operation.operator](operation.num1, operation.num2).toString();
+		operation.num2 = '0';
+		display.textContent = operation.num1;
+		inputIndex = 2;
+	}
+	if ((inputIndex === 3 && input === 'times') || (inputIndex === 3 && input === 'divide')) {
+		storeOperation(input);
+		return;
+	}
+	if (inputIndex === 4) {
+		operation.num2 = performOperation[operation.highOperator](operation.num2, operation.num3).toString();
+		operation.num1 = performOperation[operation.operator](operation.num1, operation.num2).toString();
+		operation.num2 = '0';
+		operation.num3 = '0';
+		operation.highOperator = '';
+		display.textContent = operation.num1;
+		inputIndex = 2;
+	}
+	storeOperation(input);
 }
 
 function storeOperation(operator) {
-	if (index === 1) {
+	if (inputIndex === 1 || inputIndex === 2) {
 		operation.operator = operator;
-		console.log('operator: ' + operation.operator);
+		console.log(operation.operator);
 	}
-	if (index === 2) {
-		//if times or divide dont do operation yet
-		// need to to order of operations first
-		//alter num2
-		//then return to enter new number
-		if ((operation.operator != '' && operator === 'times') || (operation.operator != '' && operator === 'divide')) {
-			console.log('do this operation first: ' + operator);
-			operation.highOperator = operator;
-			index = 3;
-			return;
-		} else {
-			console.log(operation);
-			performOperation(operation);
-			operation.num1 = display.textContent;
-			operation.operator = operator;
-			operation.num2 = '0';
-			console.log(operation);
-			return;
-		}
-	}
-	operation['num' + index] != '' ? (index += 1) : (index = index);
-}
-
-function higherOperation(num2, num3) {
-	if (operation.highOperator === 'times') {
-		operation.num2 = parseFloat((parseFloat(num2) * parseFloat(num3)).toFixed(3));
-		//performOperation(operation);
-		//formatDisplayNumber(display.textContent);
-	}
-	if (operation.highOperator === 'divide') {
-		operation.num2 = parseFloat((parseInt(num2) / parseInt(num3)).toFixed(3));
-		//performOperation(operation);
-		//formatDisplayNumber(display.textContent);
+	if (inputIndex === 3) {
+		operation.highOperator = operator;
 	}
 }
 
-function performOperation(operation) {
-	if (operation.operator === 'plus') {
-		//need to remove commas before operation
-		display.textContent = parseFloat((parseFloat(operation.num1) + parseFloat(operation.num2)).toFixed(3));
-		formatDisplayNumber(display.textContent);
+let performOperation = {
+	plus: function(num1, num2) {
+		return parseFloat((parseFloat(num1) + parseFloat(num2)).toFixed(3));
+	},
+	minus: function(num1, num2) {
+		return parseFloat((parseFloat(num1) - parseFloat(num2)).toFixed(3));
+	},
+	times: function(num1, num2) {
+		return parseFloat((parseFloat(num1) * parseFloat(num2)).toFixed(3));
+	},
+	divide: function(num1, num2) {
+		return parseFloat((parseFloat(num1) / parseFloat(num2)).toFixed(3));
 	}
-	if (operation.operator === 'minus') {
-		display.textContent = parseFloat((parseFloat(operation.num1) - parseFloat(operation.num2)).toFixed(3));
-		formatDisplayNumber(display.textContent);
-	}
-	if (operation.operator === 'times') {
-		display.textContent = parseFloat((parseFloat(operation.num1) * parseFloat(operation.num2)).toFixed(3));
-		formatDisplayNumber(display.textContent);
-	}
-	if (operation.operator === 'divide') {
-		display.textContent = parseFloat((parseInt(operation.num1) / parseInt(operation.num2)).toFixed(3));
-		formatDisplayNumber(display.textContent);
-	}
-}
+};
 
-function clearCalculator() {
-	console.log('clear');
-	index = 1;
-	isDecimalActive = true;
-	isNewOperation = true;
+function resetCalculator(inputType) {
 	operation = {
 		num1: '0',
 		operator: '',
 		num2: '0',
-		num3: '0'
+		num3: '0',
+		highOperator: ''
 	};
-	pushInputToDisplay('0');
-}
-
-function formatDisplayNumber(number) {
-	console.log('format display');
-	display.textContent = number;
-	// if (number.length > 3) {
-	// 	number = number[0] + ',' + number.substr(1, 3);
-	// 	display.textContent = number;
-	// } else {
-	// 	display.textContent = number;
-	// }
+	inputIndex = 1;
+	isDecimalActive = true;
+	if (inputType === 'clear-btn') {
+		display.textContent = operation.num1;
+	}
 }
