e6a2326d78cf15ec369a13e810c51c1c63bebf7b
unknown <Bryan@SJ-BHERRERA-XPS.netgear.com>
2020-12-27 22:02:32 -0800

    limit max digits displayed

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//need to show error on operator press that makes display text > 9
 let operation = {
 	num1: '0',
 	operator: '',
@@ -8,6 +9,7 @@ let operation = {
 
 let inputIndex = 1;
 let isDecimalActive = true;
+let isValidInputLength = true;
 
 const display = document.querySelector('div .output');
 display.textContent = operation.num1;
@@ -38,6 +40,9 @@ function handleInput(e) {
 			display.textContent = performOperation[operation.operator](operation.num1, operation.num2).toString();
 		}
 		display.textContent = performOperation[operation.operator](operation.num1, operation.num2);
+		if (display.textContent.length > 9) {
+			display.textContent = 'Error';
+		}
 		resetCalculator(inputType);
 	}
 	if (inputType === 'clear-btn') {
@@ -46,33 +51,44 @@ function handleInput(e) {
 }
 
 function displayInput(input) {
-	if (inputIndex === 1) {
-		if (input === '.' || operation.num1.includes('.')) {
-			operation.num1 += input;
-		} else {
-			operation.num1 += input;
-			operation.num1 = parseInt(operation.num1, 10);
-			operation.num1 = operation.num1.toString();
-		}
+	let maxInputLength = operation['num' + inputIndex].includes('.') ? 10 : 9;
+	if (operation['num' + inputIndex].length > maxInputLength) {
+		return;
 	}
-	if (inputIndex === 2) {
-		if (input === '.' || operation.num2.toString().includes('.')) {
-			operation.num2 += input;
-		} else {
-			operation.num2 += input;
-			operation.num2 = parseInt(operation.num2, 10);
-			operation.num2 = operation.num2.toString();
-		}
+	if (input === '.' || operation['num' + inputIndex].includes('.')) {
+		operation['num' + inputIndex] += input;
+	} else {
+		operation['num' + inputIndex] += input;
+		operation['num' + inputIndex] = parseInt(operation['num' + inputIndex], 10);
+		operation['num' + inputIndex] = operation['num' + inputIndex].toString();
 	}
-	if (inputIndex === 3) {
-		if (input === '.' || operation.num3.toString().includes('.')) {
-			operation.num3 += input;
-		} else {
-			operation.num3 += input;
-			operation.num3 = parseInt(operation.num3, 10);
-			operation.num3 = operation.num3.toString();
-		}
-	}
+	// if (inputIndex === 1) {
+	// if (input === '.' || operation.num1.includes('.')) {
+	// 	operation.num1 += input;
+	// } else {
+	// 	operation.num1 += input;
+	// 	operation.num1 = parseInt(operation.num1, 10);
+	// 	operation.num1 = operation.num1.toString();
+	// }
+	// }
+	// if (inputIndex === 2) {
+	// 	if (input === '.' || operation.num2.toString().includes('.')) {
+	// 		operation.num2 += input;
+	// 	} else {
+	// 		operation.num2 += input;
+	// 		operation.num2 = parseInt(operation.num2, 10);
+	// 		operation.num2 = operation.num2.toString();
+	// 	}
+	// }
+	// if (inputIndex === 3) {
+	// 	if (input === '.' || operation.num3.toString().includes('.')) {
+	// 		operation.num3 += input;
+	// 	} else {
+	// 		operation.num3 += input;
+	// 		operation.num3 = parseInt(operation.num3, 10);
+	// 		operation.num3 = operation.num3.toString();
+	// 	}
+	// }
 
 	//Display Input
 	display.textContent = operation['num' + inputIndex];
@@ -87,10 +103,10 @@ function orderOperators(input) {
 		display.textContent = operation.num1;
 		inputIndex = 2;
 	}
-	if ((inputIndex === 3 && input === 'times') || (inputIndex === 3 && input === 'divide')) {
-		storeOperation(input);
-		return;
-	}
+	// if ((inputIndex === 3 && input === 'times') || (inputIndex === 3 && input === 'divide')) {
+	// 	storeOperation(input);
+	// 	return;
+	// }
 	if (inputIndex === 4) {
 		operation.num2 = performOperation[operation.highOperator](operation.num2, operation.num3).toString();
 		operation.num1 = performOperation[operation.operator](operation.num1, operation.num2).toString();
@@ -142,6 +158,6 @@ function resetCalculator(inputType) {
 		display.textContent = operation.num1;
 	}
 	if (inputType === 'equals') {
-		operation.num1 = display.textContent;
+		operation.num1 = display.textContent != 'Error' ? display.textContent : '0';
 	}
 }
